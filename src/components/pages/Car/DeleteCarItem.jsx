ed3428993f3af468c86b6833b7be3f55471f18c8
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-12-22 15:33:04 +0100

    Update Delete Car Item component

--- src/components/pages/Car/DeleteCarItem.jsx
+++ src/components/pages/Car/DeleteCarItem.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import './Cars.css';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  fetchCars, deleteCar, selectCars, selectCarsStatus,
+} from '../../../redux/cars/carsSlice';
 
-const DeleteCarItem = () => (
-  <>
-    <h1>Delete Car Item</h1>
-  </>
-);
+const DeleteCarItem = () => {
+  const dispatch = useDispatch();
+  const cars = useSelector(selectCars);
+  const status = useSelector(selectCarsStatus);
+
+  useEffect(() => {
+    dispatch(fetchCars());
+  }, [dispatch]);
+
+  const handleDeleteCar = async (id) => {
+    await dispatch(deleteCar(id));
+    dispatch(fetchCars());
+  };
+
+  return (
+    <div className="cars-container">
+      <h1>List of Cars</h1>
+      {status === 'loading' && <p>Loading...</p>}
+      {status === 'failed' && <p>Error loading cars.</p>}
+      {status === 'succeeded' && (
+        <div>
+          <ul>
+            {cars.map((car) => (
+              <li key={car.id} className="car-item">
+                {car.name}
+                {' '}
+                - $
+                {car.cost}
+                {' '}
+                {car.availability ? 'available' : 'not available'}
+                {' '}
+                {car.photo}
+                <button type="button" onClick={() => handleDeleteCar(car.id)}>Delete</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default DeleteCarItem;
