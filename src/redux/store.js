f575c34bc6e66c517357ab306d23ed2c9ccaf0f2
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-12-22 15:41:19 +0100

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
+import carsReducer from './cars/carsSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    cars: carsReducer,
   },
 });
 
