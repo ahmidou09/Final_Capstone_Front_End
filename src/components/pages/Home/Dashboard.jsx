59001c49e9d1c85e298b610ecc32c10d64b60242
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-12-22 15:38:04 +0100

    Remove logout functionality from Dashboard

--- src/components/pages/Home/Dashboard.jsx
+++ src/components/pages/Home/Dashboard.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { logout } from '../../../redux/user/userSlice';
 import Cars from '../Car/Cars';
 
-const Dashboard = () => {
-  const dispatch = useDispatch();
-  const handleLogout = () => {
-    dispatch(logout());
-  };
-  return (
-    <>
-      <button type="button" onClick={handleLogout}>Log Out</button>
-      <Cars />
-    </>
-  );
-};
+const Dashboard = () => (
+  <div className="home">
+    <Cars />
+  </div>
+);
 
 export default Dashboard;
