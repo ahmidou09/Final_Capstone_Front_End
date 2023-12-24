140a29f97caaa0af39985d61d0c845bc8a0d633f
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-12-24 20:02:48 +0100

    Update Navigation component with social media icons and styling

--- src/components/shared/Navigation.jsx
+++ src/components/shared/Navigation.jsx
@@ -1,33 +1,53 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { FaTwitter, FaFacebookF } from 'react-icons/fa6';
+import { TfiGoogle } from 'react-icons/tfi';
+import { FaPinterestP } from 'react-icons/fa';
 import { logout } from '../../redux/user/userSlice';
+import logo from '../../assets/logo.png';
 
 const Navigation = () => {
   const dispatch = useDispatch();
   const handleLogout = () => {
     dispatch(logout());
   };
+
   return (
     <nav className="navigation">
-      <ul>
-        <li>
-          <Link to="/dashboard">Home</Link>
+
+      <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo" />
+      </div>
+
+      <ul className="navigation-list">
+        <li className="navigation-item">
+          <NavLink to="/dashboard">Home</NavLink>
         </li>
-        <li>
-          <Link to="/reserve-form">Reserve Form</Link>
+        <li className="navigation-item">
+          <NavLink to="/reserve-form">Reserve Form</NavLink>
         </li>
-        <li>
-          <Link to="/my-reservations">My Reservations</Link>
+        <li className="navigation-item">
+          <NavLink to="/my-reservations">My Reservations</NavLink>
         </li>
-        <li>
-          <Link to="/add-car-item">Add Car Item</Link>
+        <li className="navigation-item">
+          <NavLink to="/add-car-item">Add Car Item</NavLink>
         </li>
-        <li>
-          <Link to="/delete-car-item">Delete Car Item</Link>
+        <li className="navigation-item">
+          <NavLink to="/delete-car-item">Delete Car Item</NavLink>
         </li>
       </ul>
-      <button type="button" onClick={handleLogout}>Log Out</button>
+
+      <button type="button" className="btn log-out" onClick={handleLogout}>Log Out</button>
+
+      <ul className="social-media-icons">
+        <li><FaFacebookF /></li>
+        <li><FaTwitter /></li>
+        <li><TfiGoogle /></li>
+        <li><FaPinterestP /></li>
+      </ul>
+
+      <div className="copyright"><p>&copy; 2023 cruise fleet</p></div>
     </nav>
   );
 };
