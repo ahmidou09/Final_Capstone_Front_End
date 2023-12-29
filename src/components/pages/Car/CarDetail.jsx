615862540db2ac8fc69e630a5264c7e53aa794b2
Armynerh <aimenah@outlook.com>
2023-12-29 04:19:32 -0500

    fix errors

--- src/components/pages/Car/CarDetail.jsx
+++ src/components/pages/Car/CarDetail.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
 
-const CarDetail = () => (
-  <div>
-    Car Specific Details!
-  </div>
-);
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { selectCarDetails } from '../../../redux/cars/carsSlice';
 
+import CustomArrow from '../../shared/CustomArrow';
+import './style/CarDetail.css';
+
+const CarDetail = () => {
+  const { carId } = useParams();
+  const car = useSelector((state) => selectCarDetails(state, Number(carId)));
+
+  if (!car) {
+    return (
+      <div>
+        <h3>car not found!</h3>
+      </div>
+    );
+  }
+
+  return (
+    <div className='divider'>
+      <div className="car-img margin">
+      <img src={car.photo} alt={car.name} />
+      </div>
+      <div>
+        <h3>
+        {car.name}
+        </h3>
+        <p>-$350 deposit on any purchase</p>
+
+        <div>
+          {car.description}
+        </div>
+        <div>
+          {car.price}
+        </div>
+      </div>
+     
+    </div>
+  );
+};
 export default CarDetail;
