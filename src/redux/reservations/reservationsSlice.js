47ab7537cb28f604974588a6f1c3bbf6691c4776
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-01-04 20:38:30 +0100

    Update carsSlice to handle fetching cars API response correctly

--- src/redux/reservations/reservationsSlice.js
+++ src/redux/reservations/reservationsSlice.js
@@ -1,42 +1,86 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+
+const baseUrl = 'http://127.0.0.1:3000';
 
 const initialState = {
   reservations: [],
-  created: false,
   status: 'idle',
   error: null,
 };
 
-export const reserve = createAsyncThunk('reservations/createReservation', async (payload) => {
+export const createReserve = createAsyncThunk('reservations/createReserve', async (reservationData) => {
   try {
-    const response = await axios.post('http://localhost:3000/reservations', (payload));
-    return response.data;
+    const response = await fetch(`${baseUrl}/users/${reservationData.user_id}/reservations`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({ reservation: reservationData }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
   } catch (error) {
-    throw new Error(error);
+    throw new Error(error.message);
   }
 });
 
-export const reservationSlice = createSlice({
+export const getReservations = createAsyncThunk('reservations/getReservations', async (userId) => {
+  try {
+    const response = await fetch(`${baseUrl}/users/${userId}/reservations`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+});
+
+export const reservationsSlice = createSlice({
   name: 'reservations',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(reserve.pending, (state) => {
+      .addCase(createReserve.pending, (state) => {
         state.status = 'loading';
       })
-
-      .addCase(reserve.fulfilled, (state, action) => {
+      .addCase(createReserve.fulfilled, (state, action) => {
         state.status = 'succeeded';
-        if (action.payload.result === 'Reservation created succesfully!') {
-          state.created = true;
-        }
+        state.reservations.push(action.payload);
       })
-
-      .addCase(reserve.rejected, (state, action) => {
+      .addCase(createReserve.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
+      .addCase(getReservations.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(getReservations.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.reservations = action.payload;
+      })
+      .addCase(getReservations.rejected, (state, action) => {
         state.status = 'failed';
         state.error = action.error.message;
       });
   },
 });
+
+export const selectReservations = (state) => state.reservations.reservations;
+export const selectReservationsStatus = (state) => state.reservations.status;
+export default reservationsSlice.reducer;
