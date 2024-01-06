7b579c420c73ff29b95b712993201ed34f2b50f2
alexansaa <alexan.saa@gmail.com>
2024-01-06 15:52:10 -0500

    Init Testing

---
+++ src/test/CarsList.test.js
@@ -0,0 +1,7 @@
+// import { render, screen } from '@testing-library/react';
+// import CarsList from '../components/pages/Car/CarsList';
+
+// test('renders cars list', () => {
+//   render(<CarsList />);
+//   const 
+// })
